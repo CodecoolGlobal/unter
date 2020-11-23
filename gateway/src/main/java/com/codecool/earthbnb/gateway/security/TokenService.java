11a750f7d00163892edfbd1ebb1611266299a289
dr. Simon Bálint <balladin0@gmail.com>
2020-11-23 14:43:30 +0100

    Add basic JWT services with partial security config

---
+++ gateway/src/main/java/com/codecool/earthbnb/gateway/security/TokenService.java
@@ -0,0 +1,92 @@
+package com.codecool.earthbnb.gateway.security;
+
+import io.jsonwebtoken.*;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Data
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class TokenService {
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey = "secret";
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 36000000; // 10 hours
+
+    private final String rolesFieldName = "roles";
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username, List<String> roles) {
+        // Add a custom field to the token
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(rolesFieldName, roles);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public Optional<Cookie> getTokenCookieFromRequest(HttpServletRequest request) {
+        if (request.getCookies() == null) return Optional.empty();
+        System.out.println("Cookies: " + request.getCookies().toString());
+        return Arrays.stream(request.getCookies())
+                .filter(cookie -> "JWT".equals(cookie.getName()))
+                .findFirst();
+    }
+
+    public String getTokenFromRequest(HttpServletRequest request) {
+        return getTokenCookieFromRequest(request).map(Cookie::getValue).orElse(null);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return claims.getBody().getExpiration().after(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            log.debug("JWT token invalid " + e);
+        }
+        return false;
+    }
+
+    public String getUsernameFromToken(HttpServletRequest req) {
+        String mycookie = null;
+        Cookie[] cookies = req.getCookies();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("authentication") && validateToken(getTokenFromRequest(req))) {
+                    mycookie = cookie.getValue();
+                    Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(mycookie).getBody();
+                    return body.getSubject();
+                }
+            }
+        }
+
+        return null;
+    }
+}
