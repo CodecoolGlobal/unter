f389e3fc8ad52b3d7432f6bd2786b4982efd9085
magfabian <mag.fabian87@gmail.com>
2020-11-24 13:27:28 +0100

    AccommodationApplication refactor

--- accomodation/src/main/java/com/codecool/accomodation/AccommodationApplication.java
+++ accomodation/src/main/java/com/codecool/accomodation/AccommodationApplication.java
@@ -2,12 +2,14 @@ package com.codecool.accomodation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class AccomodationApplication {
+@EnableEurekaClient
+public class AccommodationApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AccomodationApplication.class, args);
+        SpringApplication.run(AccommodationApplication.class, args);
     }
 
 }
