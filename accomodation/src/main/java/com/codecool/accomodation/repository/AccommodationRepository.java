b502a4cfdb58df494e574210d106daf971161c75
magfabian <mag.fabian87@gmail.com>
2020-11-25 15:12:33 +0100

    Define methods in AccommodationRepository

--- accomodation/src/main/java/com/codecool/accomodation/repository/AccommodationRepository.java
+++ accomodation/src/main/java/com/codecool/accomodation/repository/AccommodationRepository.java
@@ -1,7 +1,12 @@
 package com.codecool.accomodation.repository;
 
-import com.codecool.accomodation.entity.Accommodation;
+import com.codecool.accomodation.model.entity.Accommodation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccommodationRepository extends JpaRepository<Accommodation, Long> {
+
+    List<Accommodation> findAccommodationsByHostHostId(Long hostId);
+    void deleteAccommodationById(Long accommodationId);
 }
