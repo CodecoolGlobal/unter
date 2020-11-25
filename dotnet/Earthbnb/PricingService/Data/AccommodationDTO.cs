0518671fc0b36d8447e38e8b1d3f10c68e8d6212
Rhyvan <polyakan@gmail.com>
2020-11-25 16:03:34 +0100

    Pricing service: CRUD api, DTO

---
+++ dotnet/Earthbnb/PricingService/Data/AccommodationDTO.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using PricingService.Models;
+
+namespace PricingService.Data
+{
+#nullable enable
+
+    public record AccommodationDTO
+    {
+        public string? AccommodationId { get; set; }
+        [Required]
+        public decimal BasePrice { get; set; }
+        [Required]
+        [StringLength(3)]
+        public string CurrencyISO { get; set; }
+        public ICollection<PriceIntervalDTO> PriceIntervals { get; set; } = new List<PriceIntervalDTO>();
+
+
+        public Accommodation ConvertToModel()
+        {
+            var obj = new Accommodation()
+            {
+                BasePrice = BasePrice,
+                CurrencyISO = CurrencyISO,
+            };
+
+            if (PriceIntervals != null)
+            {
+                obj.PriceIntervals = PriceIntervals.Select(x => x.ConvertToModel()).ToList();
+            }
+
+            return obj;
+        }
+
+        public AccommodationDTO ConvertFromModel(Accommodation model)
+        {
+            return new AccommodationDTO()
+            {
+                AccommodationId = model.AccommodationId,
+                BasePrice = model.BasePrice,
+                CurrencyISO = model.CurrencyISO,
+                PriceIntervals = model.PriceIntervals
+                    .Select(pi => new PriceIntervalDTO(pi.StartDate, pi.EndDate, pi.Price) { PriceIntervalId = pi.PriceIntervalId })
+                    .ToList()
+            };
+        }
+    }
+
+    public record PriceIntervalDTO(
+        DateTime StartDate,
+        DateTime EndDate,
+        decimal Price)
+    {
+        public string? PriceIntervalId { get; set; }
+        public string? AccommodationId { get; set; }
+
+
+
+        public PriceInterval ConvertToModel()
+        {
+            return new PriceInterval()
+            {
+                StartDate = this.StartDate,
+                EndDate = this.EndDate,
+                Price = this.Price
+            };
+        }
+
+        public PriceIntervalDTO ConvertFromModel(PriceInterval model)
+        {
+            return new PriceIntervalDTO(
+                model.StartDate,
+                model.EndDate,
+                model.Price)
+            {
+                PriceIntervalId = model.PriceIntervalId,
+                AccommodationId = model.AccommodationId
+            };
+        }
+
+        public void OverwriteRefModel(ref PriceInterval model)
+        {
+            model.StartDate = StartDate;
+            model.EndDate = EndDate;
+            model.Price = Price;
+        }
+
+    }
+
+    public static partial class Converter
+    {
+        public static AccommodationDTO ConvertModelToAccommodationDTO(Accommodation model)
+        {
+            return new AccommodationDTO()
+            {
+                AccommodationId = model.AccommodationId,
+                BasePrice = model.BasePrice,
+                CurrencyISO = model.CurrencyISO,
+                PriceIntervals = model.PriceIntervals
+                    .Select(pi => new PriceIntervalDTO(pi.StartDate, pi.EndDate, pi.Price) { PriceIntervalId = pi.PriceIntervalId })
+                    .ToList()
+            };
+        }
+    }
+
+
+
+}
---
+++ dotnet/Earthbnb/PricingService/Data/AccommodationSeeder.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PricingService.Models;
+
+namespace PricingService.Data
+{
+    public class AccommodationSeeder
+    {
+        public static List<Accommodation> GetGeneratedAccommodations()
+        {
+            return new List<Accommodation>()
+            {
+                new Accommodation()
+                {
+                    BasePrice = 500,
+                    CurrencyISO = "USD",
+                    PriceIntervals = new List<PriceInterval>()
+                    {
+                        new PriceInterval
+                        {
+                            StartDate = new DateTime(2020, 06, 1),
+                            EndDate = new DateTime(2020, 09, 1),
+                            Price = 1000
+                        },
+                        new PriceInterval
+                        {
+                            StartDate = new DateTime(2020, 12, 1),
+                            EndDate = new DateTime(2020, 12, 31),
+                            Price = 800
+                        }
+                    }
+                },
+                new Accommodation()
+                {
+                    BasePrice = 5000,
+                    CurrencyISO = "HUF",
+                    PriceIntervals = new List<PriceInterval>()
+                    {
+                        new PriceInterval
+                        {
+                            StartDate = new DateTime(2020, 7, 1),
+                            EndDate = new DateTime(2020, 8, 31),
+                            Price = 6000
+                        },
+                        new PriceInterval
+                        {
+                            StartDate = new DateTime(2020, 12, 1),
+                            EndDate = new DateTime(2020, 12, 31),
+                            Price = 10000
+                        },
+                        new PriceInterval
+                        {
+                            StartDate = new DateTime(2020, 04, 1),
+                            EndDate = new DateTime(2020, 04, 10),
+                            Price = 8000
+                        }
+
+                    }
+                },
+                new Accommodation()
+                {
+                    BasePrice = 8000,
+                    CurrencyISO = "HUF",
+                    PriceIntervals = new List<PriceInterval>()
+                    {
+                        new PriceInterval
+                        {
+                            StartDate = new DateTime(2020, 1, 1),
+                            EndDate = new DateTime(2020, 5, 31),
+                            Price = 5000
+                        }
+                    }
+                },
+            };
+        }
+    }
+}
