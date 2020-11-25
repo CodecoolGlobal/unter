aee4d5e5e85ff105573831a0533a1f800ec75a3a
magfabian <mag.fabian87@gmail.com>
2020-11-25 15:11:38 +0100

    Implement AccommodationDB

--- accomodation/src/main/java/com/codecool/accomodation/service/DAO/AccommodationDB.java
+++ accomodation/src/main/java/com/codecool/accomodation/service/DAO/AccommodationDB.java
@@ -1,9 +1,14 @@
 package com.codecool.accomodation.service.DAO;
 
+import com.codecool.accomodation.model.DTO.AccommodationDTO;
+import com.codecool.accomodation.model.entity.Accommodation;
 import com.codecool.accomodation.repository.AccommodationRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @Component
 @Data
@@ -11,4 +16,47 @@ import org.springframework.stereotype.Component;
 public class AccommodationDB implements AccommodationDAO {
 
     private final AccommodationRepository repository;
+
+    @Override
+    public List<Accommodation> findAllByHost(Long hostId) {
+        return repository.findAccommodationsByHostHostId(hostId);
+    }
+
+    @Override
+    public void saveNewAccommodation(AccommodationDTO accommodationDTO) {
+        Accommodation accommodation = Accommodation.builder()
+            .host(accommodationDTO.getHost())
+            .name(accommodationDTO.getName())
+            .description(accommodationDTO.getDescription())
+            .location(accommodationDTO.getLocation())
+            .type((accommodationDTO.getType()))
+            .maxNumberOfGuests(accommodationDTO.getMaxNumberOfGuest())
+            .rooms(accommodationDTO.getRooms())
+            .build();
+
+        repository.save(accommodation);
+    }
+
+    @Override
+    public void deleteAccommodation(Long accommodationId) {
+        repository.deleteAccommodationById(accommodationId);
+    }
+
+    @Override
+    public Accommodation findAccommodationById(Long accommodationId) {
+        return repository.findById(accommodationId)
+            .orElseThrow(() -> new NoSuchElementException("No accommondation was found"));
+    }
+
+    @Override
+    public void updateAccommodation(String accommodationId, AccommodationDTO accommodationDTO) {
+        Accommodation toEdit = findAccommodationById(Long.parseLong(accommodationId));
+
+        toEdit.setName(accommodationDTO.getName());
+        toEdit.setDescription(accommodationDTO.getDescription());
+        toEdit.setMaxNumberOfGuests(accommodationDTO.getMaxNumberOfGuest());
+        toEdit.setRooms(accommodationDTO.getRooms());
+
+        repository.save(toEdit);
+    }
 }
