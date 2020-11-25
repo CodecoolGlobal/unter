c7ed401dd13765807b9d6b5fb4ce3fc92bea11c5
magfabian <mag.fabian87@gmail.com>
2020-11-25 17:15:14 +0100

    Test add new repository

---
+++ accomodation/src/test/java/com/codecool/accomodation/repository/AccommodationRepositoryTest.java
@@ -0,0 +1,57 @@
+package com.codecool.accomodation.repository;
+
+import com.codecool.accomodation.model.entity.Accommodation;
+import com.codecool.accomodation.model.entity.Address;
+import com.codecool.accomodation.model.entity.Host;
+import com.codecool.accomodation.model.entity.Location;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class AccommodationRepositoryTest {
+
+    @Autowired
+    private AccommodationRepository repository;
+
+    @Test
+    public void test_saveNewAccommodation_hasSizeOne() {
+
+        Address address = Address.builder()
+            .city("Kazincbarcika")
+            .street("Utca")
+            .zipCode("4444")
+            .houseNumber(12)
+            .build();
+
+        Location location = Location.builder()
+            .address(address)
+            .description("Nice!")
+            .latitude(22.00)
+            .longitude(32.00)
+            .build();
+
+        Accommodation accommodation = Accommodation.builder()
+            .description("Nice!444négy")
+            .location(location)
+            .maxNumberOfGuests(4000)
+            .name("Házikó")
+            .build();
+
+        repository.save(accommodation);
+
+        List<Accommodation> accommodations = repository.findAll();
+
+        assertThat(accommodations).hasSize(1);
+    }
+}
