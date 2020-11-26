8080e707ea66f35b32e1bc16b0e9c41ac7cc792e
magfabian <mag.fabian87@gmail.com>
2020-11-26 10:44:34 +0100

    Test if name of accommodation is null

--- accomodation/src/test/java/com/codecool/accomodation/repository/AccommodationRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/AccommodationRepositoryTest.java
@@ -2,20 +2,20 @@ package com.codecool.accomodation.repository;
 
 import com.codecool.accomodation.model.entity.Accommodation;
 import com.codecool.accomodation.model.entity.Address;
-import com.codecool.accomodation.model.entity.Host;
 import com.codecool.accomodation.model.entity.Location;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -53,5 +53,14 @@ class AccommodationRepositoryTest {
         List<Accommodation> accommodations = repository.findAll();
 
         assertThat(accommodations).hasSize(1);
+    }
+
+    @Test
+    public void test_accommodationNameShouldBeNotNull_ThrowsException() {
+        Accommodation accommodation = Accommodation.builder()
+            .build();
+
+        assertThrows(DataIntegrityViolationException.class,
+            () -> repository.save(accommodation));
     }
 }
