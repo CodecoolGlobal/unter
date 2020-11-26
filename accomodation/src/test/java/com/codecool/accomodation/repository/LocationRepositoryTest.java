d724396f2fdbb647ca3a9a5d6866c508c55a6200
dr. Simon Bálint <balladin0@gmail.com>
2020-11-26 18:52:53 +0100

    Add dirty context for test with after each test

--- accomodation/src/test/java/com/codecool/accomodation/repository/LocationRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/LocationRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -17,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class LocationRepositoryTest {
 
     @Autowired
