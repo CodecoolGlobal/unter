c1278e9cfdc6610cb660c9765800a0fbb804af35
magfabian <mag.fabian87@gmail.com>
2020-11-26 12:24:08 +0100

    Test room repository save

--- accomodation/src/main/java/com/codecool/accomodation/model/entity/Room.java
+++ accomodation/src/main/java/com/codecool/accomodation/model/entity/Room.java
@@ -28,5 +28,4 @@ public class Room {
 
     @ManyToOne
     private Accommodation accommodation;
-
 }
--- accomodation/src/test/java/com/codecool/accomodation/repository/RoomRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/RoomRepositoryTest.java
@@ -1,13 +1,29 @@
 package com.codecool.accomodation.repository;
 
+import com.codecool.accomodation.model.entity.Room;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class RoomRepositoryTest {
 
+    @Autowired
+    private RoomRepository repository;
+
+    @Test
+    public void test_saveNewRoom_hasSizeOne() {
+        Room room = Room.builder().build();
+        repository.saveAndFlush(room);
+
+        List<Room> rooms = repository.findAll();
+        assertThat(rooms).hasSize(1);
+    }
 }
