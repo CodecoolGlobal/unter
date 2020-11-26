cf6b3ba8c9e1c6557d8c03dbfb41c01265f3a005
magfabian <mag.fabian87@gmail.com>
2020-11-26 12:04:18 +0100

    Delete unnecessary import

--- accomodation/src/test/java/com/codecool/accomodation/repository/HostRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/HostRepositoryTest.java
@@ -5,14 +5,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -24,7 +23,7 @@ public class HostRepositoryTest {
     @Test
     public void test_saveNewHost_hasSizeOne() {
         Host host = Host.builder()
-            .email("host@earth.com")
+            .email("host1@earth.com")
             .phone("0690666999")
             .build();
 
@@ -32,5 +31,43 @@ public class HostRepositoryTest {
 
         List<Host> hosts = repository.findAll();
         assertThat(hosts).hasSize(1);
+    }
+
+    @Test
+    public void test_saveHostsWithSameEmail_ThrowsException() {
+        Host host = Host.builder()
+            .email("host2@earth.com")
+            .phone("0690666999")
+            .build();
+
+        repository.saveAndFlush(host);
+
+        Host host2 = Host.builder()
+            .email("host2@earth.com")
+            .phone("0690666888")
+            .build();
+
+        assertThrows(DataIntegrityViolationException.class, () ->
+            repository.saveAndFlush(host2));
+    }
+
+    @Test
+    public void test_hostEmailShouldBeNotNull_ThrowsException() {
+        Host host = Host.builder()
+            .phone("0690666999")
+            .build();
+
+        assertThrows(DataIntegrityViolationException.class, () ->
+            repository.saveAndFlush(host));
+    }
+
+    @Test
+    public void test_hostPhoneShouldBeNotNull_ThrowsException() {
+        Host host = Host.builder()
+            .email("host@earth.com")
+            .build();
+
+        assertThrows(DataIntegrityViolationException.class, () ->
+            repository.saveAndFlush(host));
     }
 }
