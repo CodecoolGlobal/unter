4ef2fac1bebaee58fe320639914f4b1c13331d9f
magfabian <mag.fabian87@gmail.com>
2020-11-26 12:17:47 +0100

    Add Annotations and imports

--- accomodation/src/test/java/com/codecool/accomodation/repository/RoomRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/RoomRepositoryTest.java
@@ -1,8 +1,13 @@
 package com.codecool.accomodation.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class RoomRepositoryTest {
 
 }
