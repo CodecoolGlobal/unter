f06f76093079096eddaac765977631276382352f
magfabian <mag.fabian87@gmail.com>
2020-11-26 12:15:53 +0100

    Refactor imports

--- accomodation/src/test/java/com/codecool/accomodation/repository/LocationRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/LocationRepositoryTest.java
@@ -1,6 +1,14 @@
 package com.codecool.accomodation.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.codecool.accomodation.model.entity.Location;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
