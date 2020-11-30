196f33f95a9cbd8de20acb82c41e19ccb448c46d
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-11-30 20:51:35 +0100

    DTOWrapper annotated with @AllargsConstructior
    DTOCreator has a private "converter" method and a public linker method that accepts a List of Accommodations

--- accomodation/src/main/java/com/codecool/accomodation/model/DTO/DTOWrapper.java
+++ accomodation/src/main/java/com/codecool/accomodation/model/DTO/DTOWrapper.java
@@ -1,10 +1,12 @@
 package com.codecool.accomodation.model.DTO;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class DTOWrapper {
 
     private List<AccommodationDTO> accommodationDTOS;
