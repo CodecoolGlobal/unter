317cd596842968eb96b5db9463ab1ff80b1d13b0
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-11-30 20:07:00 +0100

    created DTO Wrapper class with a private field: List<AccomodationDTO>

---
+++ accomodation/src/main/java/com/codecool/accomodation/model/DTO/DTOWrapper.java
@@ -0,0 +1,11 @@
+package com.codecool.accomodation.model.DTO;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DTOWrapper {
+
+    private List<AccommodationDTO> accommodationDTOS;
+}
