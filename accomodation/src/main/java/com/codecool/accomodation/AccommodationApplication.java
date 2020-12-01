fb02717b290326745f2add7a69880488f0fb6346
Gergo1212 <gergo.meszaros.codecool@gmail.com>
2020-12-01 08:13:39 +0100

    added Modelmapper creator method to AccommodationApplication.java

--- accomodation/src/main/java/com/codecool/accomodation/AccommodationApplication.java
+++ accomodation/src/main/java/com/codecool/accomodation/AccommodationApplication.java
@@ -1,8 +1,10 @@
 package com.codecool.accomodation;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
@@ -14,4 +16,8 @@ public class AccommodationApplication {
         SpringApplication.run(AccommodationApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper mapperCreator(){
+        return new ModelMapper();
+    }
 }
