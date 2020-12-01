4deea8206362da43b88530b268baad9c1a633596
magfabian <mag.fabian87@gmail.com>
2020-12-01 14:28:56 +0100

    Remove unnecessary imports

--- accomodation/src/main/java/com/codecool/accomodation/repository/LocationRepository.java
+++ accomodation/src/main/java/com/codecool/accomodation/repository/LocationRepository.java
@@ -4,6 +4,8 @@ import com.codecool.accomodation.model.entity.Coordinate;
 import com.codecool.accomodation.model.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Location getByCoordinate(Coordinate coordinate);
 }
