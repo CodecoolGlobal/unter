b9003ba5216892e5a43a401015920c8e52b1d9bf
magfabian <mag.fabian87@gmail.com>
2020-12-01 14:29:57 +0100

    Add CoordinateRepositoryTest and do smoke test

---
+++ accomodation/src/test/java/com/codecool/accomodation/repository/CoordinateRepositoryTest.java
@@ -0,0 +1,27 @@
+package com.codecool.accomodation.repository;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class CoordinateRepositoryTest {
+
+    @Autowired
+    CoordinateRepository coordinateRepository;
+
+    @Test
+    public void smokeTest() {
+        assertThat(coordinateRepository).isNotNull();
+    }
+}
