c0d0fec2bf4efd24e5d907019fa066bda9463d9e
magfabian <mag.fabian87@gmail.com>
2020-12-01 15:07:26 +0100

    Remove unnecessary imports

--- accomodation/src/test/java/com/codecool/accomodation/repository/CoordinateRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/CoordinateRepositoryTest.java
@@ -5,15 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
