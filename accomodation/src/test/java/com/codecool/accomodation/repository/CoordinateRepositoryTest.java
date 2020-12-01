f6550643c5ef7554c681c0cc123638d51bda8897
magfabian <mag.fabian87@gmail.com>
2020-12-01 14:36:03 +0100

    Test saving new coordinates

--- accomodation/src/test/java/com/codecool/accomodation/repository/CoordinateRepositoryTest.java
+++ accomodation/src/test/java/com/codecool/accomodation/repository/CoordinateRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.codecool.accomodation.repository;
 
+import com.codecool.accomodation.model.entity.Coordinate;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,5 +27,20 @@ public class CoordinateRepositoryTest {
     @Test
     public void smokeTest() {
         assertThat(coordinateRepository).isNotNull();
+    }
+
+    @Test
+    public void test_saveNewCoordinate_hasSizeIncreasesWithOne() {
+        List<Coordinate> originalData = coordinateRepository.findAll();
+        Integer originalDataSize = originalData.size();
+
+        Coordinate coordinate = Coordinate.builder()
+            .longitude(11.2)
+            .latitude(45.45)
+            .build();
+
+        coordinateRepository.save(coordinate);
+        List<Coordinate> coordinates = coordinateRepository.findAll();
+        assertThat(coordinates).hasSize(originalDataSize + 1);
     }
 }
