ac6bf2b74e92ce3f05215fcd9f291f9d9373bae2
magfabian <mag.fabian87@gmail.com>
2020-12-08 13:58:06 +0100

    Remove unnecessary imports

--- accomodation/src/test/java/com/codecool/accommodation/controller/AccommodationControllerTest.java
+++ accomodation/src/test/java/com/codecool/accommodation/controller/AccommodationControllerTest.java
@@ -1,21 +1,23 @@
 package com.codecool.accommodation.controller;
 
+import com.codecool.accommodation.model.DTO.AccommodationDTO;
 import com.codecool.accommodation.model.entity.*;
 import com.codecool.accommodation.service.AccommodationService;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
