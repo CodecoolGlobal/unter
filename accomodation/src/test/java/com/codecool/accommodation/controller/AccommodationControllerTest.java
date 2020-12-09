dad86ddb39cbededaaad522592ab5cbcac3a3567
magfabian <mag.fabian87@gmail.com>
2020-12-09 13:20:10 +0100

    Add new imports

--- accomodation/src/test/java/com/codecool/accommodation/controller/AccommodationControllerTest.java
+++ accomodation/src/test/java/com/codecool/accommodation/controller/AccommodationControllerTest.java
@@ -4,6 +4,8 @@ import com.codecool.accommodation.model.DTO.AccommodationDTO;
 import com.codecool.accommodation.model.entity.*;
 import com.codecool.accommodation.service.AccommodationService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jfr.ContentType;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,7 +22,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest
