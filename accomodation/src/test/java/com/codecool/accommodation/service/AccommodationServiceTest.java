9ad528a60b8bdef407815f118f383c06bc836f48
magfabian <mag.fabian87@gmail.com>
2020-12-09 13:30:19 +0100

    Add AccommodationServiceTest

---
+++ accomodation/src/test/java/com/codecool/accommodation/service/AccommodationServiceTest.java
@@ -0,0 +1,17 @@
+package com.codecool.accommodation.service;
+
+import com.codecool.accommodation.repository.AccommodationRepository;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+
+@SpringBootTest
+public class AccommodationServiceTest {
+
+    @InjectMocks
+    private AccommodationService service;
+
+    @MockBean
+    private AccommodationRepository repository;
+}
