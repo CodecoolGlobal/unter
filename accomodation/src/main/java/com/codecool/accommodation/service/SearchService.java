8897592b8d1c7042bebf641cdc4f35644be3777e
dr. Simon Bálint <balladin0@gmail.com>
2020-12-12 18:46:21 +0100

    Add service to get accommodations in given distance from a coordinate

--- accomodation/src/main/java/com/codecool/accommodation/service/SearchService.java
+++ accomodation/src/main/java/com/codecool/accommodation/service/SearchService.java
@@ -1,28 +1,40 @@
 package com.codecool.accommodation.service;
 
+import com.codecool.accommodation.model.DTO.AccommodationDTO;
+import com.codecool.accommodation.model.DTO.CoordinateDTO;
+import com.codecool.accommodation.model.entity.Accommodation;
+import com.codecool.accommodation.model.entity.Coordinate;
+import com.codecool.accommodation.model.wrapper.AccommodationDTOWrapper;
+import com.codecool.accommodation.service.DAO.CoordinateDAO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SearchService {
-//    private final DTOCreator creator;
-//    private final CoordinateDAO coordinateDAO;
-//    private final LocationDAO locationDAO;
-//
-//    public DTOWrapper getAllAccommodationInRadius(CoordinateDTO coordinate, Double searchRadius) {
-//        List<Coordinate> coordinatesWithinDistance = coordinateDAO.getAllByDistanceFromCoordinate(coordinate, searchRadius);
-//        if (coordinatesWithinDistance.isEmpty()) return creator.turnInputListToDTO(Arrays.asList(new Accommodation()));
-//
-//        List<Accommodation> allAccommodationsInRadius = new ArrayList<>();
-//        System.out.println(coordinatesWithinDistance);
-//        for (Coordinate actualCoordinate : coordinatesWithinDistance) {
-//            Location actualLocation = locationDAO.getLocationByCoordinate(actualCoordinate);
-//
-//            allAccommodationsInRadius.add(actualLocation.getAccommodation());
-//        }
-//        return creator.turnInputListToDTO(allAccommodationsInRadius);
-//    }
+    private final DTOCreator creator;
+    private final CoordinateDAO coordinateDAO;
 
+    public ResponseEntity<?> getAccommodationsInRadius(CoordinateDTO coordinate, Double searchRadius) {
+        List<Coordinate> foundCoordinates = coordinateDAO.getAllByDistanceFromCoordinate(coordinate, searchRadius);
+        if (foundCoordinates.isEmpty())
+            return ResponseEntity.ok(
+                    new AccommodationDTOWrapper(Collections.singletonList(
+                            AccommodationDTO.builder()
+                                    .coordinates(new CoordinateDTO(null, null))
+                                    .build())
+                    )
+            );
 
+        List<Accommodation> allAccommodationsInRadius = new ArrayList<>();
+        for (Coordinate actualCoordinate : foundCoordinates) {
+            allAccommodationsInRadius.add(actualCoordinate.getAccommodation());
+        }
+        return ResponseEntity.ok(creator.turnInputListToAccommodationDTO(allAccommodationsInRadius));
+    }
 }
