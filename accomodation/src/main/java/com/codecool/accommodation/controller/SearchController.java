f0176b32199af7b7d84fcc50af7b6e93df3e073b
dr. Simon Bálint <balladin0@gmail.com>
2020-12-12 18:18:08 +0100

    Modify search controller to handle errors
    Search radius is not required, so no new endpoint is needed for defaulb radius searches.
    Return response entity instead of wrapper class, so can send back error codes.
    Endpoint produces JSON with UTF-8, so Hungarian characters are not encoded when converting to ResponseEntity.

--- accomodation/src/main/java/com/codecool/accommodation/controller/SearchController.java
+++ accomodation/src/main/java/com/codecool/accommodation/controller/SearchController.java
@@ -1,9 +1,9 @@
 package com.codecool.accommodation.controller;
 
 import com.codecool.accommodation.model.DTO.CoordinateDTO;
-import com.codecool.accommodation.model.wrapper.AccommodationDTOWrapper;
 import com.codecool.accommodation.service.SearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SearchController {
     private final SearchService searchService;
 
-    @GetMapping
-    public AccommodationDTOWrapper simpleSearch(
+    @GetMapping(produces = "application/json;charset=utf8")
+    public ResponseEntity<?> simpleSearch(
             @RequestParam Double longitude,
             @RequestParam Double latitude,
-            @RequestParam Double radius
+            @RequestParam(required = false) Double radius
     ) {
         return searchService.getAccommodationsInRadius(new CoordinateDTO(latitude, longitude), radius);
     }
