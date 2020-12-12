736c926c200e94a62920bfc3894605747acfe8df
dr. Simon Bálint <balladin0@gmail.com>
2020-12-12 19:58:25 +0100

    Refactor distance search for accommodation

--- accomodation/src/main/java/com/codecool/accommodation/service/SearchService.java
+++ accomodation/src/main/java/com/codecool/accommodation/service/SearchService.java
@@ -1,17 +1,14 @@
 package com.codecool.accommodation.service;
 
-import com.codecool.accommodation.model.DTO.AccommodationDTO;
 import com.codecool.accommodation.model.DTO.CoordinateDTO;
 import com.codecool.accommodation.model.entity.Accommodation;
 import com.codecool.accommodation.model.entity.Coordinate;
-import com.codecool.accommodation.model.wrapper.AccommodationDTOWrapper;
 import com.codecool.accommodation.service.DAO.CoordinateDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -19,25 +16,20 @@ import java.util.List;
 public class SearchService {
     private final DTOCreator creator;
     private final CoordinateDAO coordinateDAO;
+    private static final double DEFAULT_SEARCH_DISTANCE = 0D;
+    private static final String NO_COORDINATE_MESSAGE = "Coordinates cannot be null.";
 
     public ResponseEntity<?> getAccommodationsInRadius(CoordinateDTO coordinate, Double searchRadius) {
-        searchRadius = searchRadius == null ? 0D : searchRadius;
+        searchRadius = searchRadius == null ? DEFAULT_SEARCH_DISTANCE : searchRadius;
         if (coordinate.getLatitude() == null || coordinate.getLongitude() == null)
-            return ResponseEntity.status(400).body("Coordinates cannot be null.");
-
-        List<Coordinate> foundCoordinates = coordinateDAO.getAllByDistanceFromCoordinate(coordinate, searchRadius);
-        if (foundCoordinates.isEmpty())
-            return ResponseEntity.ok(
-                    new AccommodationDTOWrapper(Collections.singletonList(
-                            AccommodationDTO.builder()
-                                    .coordinates(new CoordinateDTO(null, null))
-                                    .build())
-                    )
-            );
+            return ResponseEntity.status(400).body(NO_COORDINATE_MESSAGE);
 
         List<Accommodation> allAccommodationsInRadius = new ArrayList<>();
-        for (Coordinate actualCoordinate : foundCoordinates) {
-            allAccommodationsInRadius.add(actualCoordinate.getAccommodation());
+        List<Coordinate> foundCoordinates = coordinateDAO.getAllByDistanceFromCoordinate(coordinate, searchRadius);
+        if (!foundCoordinates.isEmpty()) {
+            for (Coordinate actualCoordinate : foundCoordinates) {
+                allAccommodationsInRadius.add(actualCoordinate.getAccommodation());
+            }
         }
         return ResponseEntity.ok(creator.turnInputListToAccommodationDTO(allAccommodationsInRadius));
     }
