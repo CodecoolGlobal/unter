e297123cd7a2d28a7bb2905e8953b1f47c5d5d4e
dr. Simon Bálint <balladin0@gmail.com>
2020-12-12 18:23:42 +0100

    Fix fields
    Coordinates are now stored in DTO, so no additional data is revealed to front-end.
    Description is added.
    Constructors are created to be able to create empty DTOs in case of empty search results without using or modifying converter service.
    If null check is implemented in converter, NoArgs and AllArgs could be deleted. However, it is not required.

--- accomodation/src/main/java/com/codecool/accommodation/model/DTO/AccommodationDTO.java
+++ accomodation/src/main/java/com/codecool/accommodation/model/DTO/AccommodationDTO.java
@@ -1,22 +1,27 @@
 package com.codecool.accommodation.model.DTO;
 
-import com.codecool.accommodation.model.entity.Coordinate;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AccommodationDTO {
     private Long id;
     private String accommodationName;
+    private String description;
     private String pictures;
     private Integer capacity;
     private Integer numberOfRooms;
     private Integer numberOfBeds;
     private Integer numberOfBathrooms;
-    private Coordinate coordinates;
+    private CoordinateDTO coordinates;
 
-//    private String description;
+    // TODO: implement these fields later
+//    private Long hostId;
 //    private AccommodationType type;
 //    private Set<Room> rooms;
 //    private Address address;
