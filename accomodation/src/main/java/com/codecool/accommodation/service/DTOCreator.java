24ae14942ca76cb82237d0625a955d467b0e00a9
dr. Simon Bálint <balladin0@gmail.com>
2020-12-14 11:35:31 +0100

    Add capacity field to dummy content builder

--- accomodation/src/main/java/com/codecool/accommodation/service/DTOCreator.java
+++ accomodation/src/main/java/com/codecool/accommodation/service/DTOCreator.java
@@ -52,7 +52,7 @@ public class DTOCreator {
                 .accommodationName(accommodation.getName())
                 .description(accommodation.getDescription())
                 .pictures(accommodation.getPictureUrl())
-//                .capacity() // TODO: implement calculate capacity
+                .capacity(accommodation.getMaxNumberOfGuests()) // TODO: implement calculate capacity
                 .numberOfRooms(accommodation.getRooms().size())
 //                .numberOfBeds() // TODO: implement calculate number of beds
 //                .numberOfBathrooms() // TODO: implement caclulate number of bathrooms
