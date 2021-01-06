59c843f67cb14106942ca6dc503afc3c4e099929
sibaluca12 <sibaluca@gmail.com>
2021-01-06 11:40:10 +0100

    secure password to default true

--- gateway/src/main/java/com/codecool/earthbnb/gateway/service/validation/ValidatorService.java
+++ gateway/src/main/java/com/codecool/earthbnb/gateway/service/validation/ValidatorService.java
@@ -40,6 +40,7 @@ public class ValidatorService {
 
     private boolean isValidPasswordSecure(String password) {
 
-        return password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$");
+        return true;
+//        return password.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,20}$");
     }
 }
