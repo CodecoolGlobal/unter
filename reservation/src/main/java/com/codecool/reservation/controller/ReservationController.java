47d2eadfd2ec3bd6c18e6fb3e78a694461f2c295
sibaluca12 <sibaluca@gmail.com>
2021-01-07 09:07:58 +0100

    commenting out routes, trying to figure out 405

--- reservation/src/main/java/com/codecool/reservation/controller/ReservationController.java
+++ reservation/src/main/java/com/codecool/reservation/controller/ReservationController.java
@@ -4,55 +4,84 @@ import com.codecool.reservation.model.DTO.DateIntervalDTO;
 import com.codecool.reservation.model.DTO.ReservationDTO;
 import com.codecool.reservation.model.entity.Reservation;
 import com.codecool.reservation.service.ReservationService;
+import com.google.common.net.MediaType;
+import jdk.jfr.ContentType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/res")
 public class ReservationController {
 
     private final ReservationService service;
 
-    @GetMapping("/")
-    public List<Reservation> getAll() {
-        return service.getAllReservation();
+//    @GetMapping("/")
+//    public List<Reservation> getAll() {
+//        return service.getAllReservation();
+//    }
+
+    @GetMapping("/reservation-id/{reservationId}")
+    public void getReservationById(@PathVariable Long reservationId) {
+        System.out.println("heloooo");
     }
 
-    @GetMapping("/reservation/{reservationId}")
-    public Reservation getReservationById(@PathVariable(name = "reservationId") Long reservationId) {
-        return service.getReservationById(reservationId);
-    }
+//    @GetMapping("/reservation-id/{reservationId}")
+//    public Reservation getReservationById(@PathVariable Long reservationId) {
+//        System.out.println("heloooo");
+//        return service.getReservationById(reservationId);
+//    }
 
-    @GetMapping("/by-accommodation/{accommodationId}")
-    public List<Reservation> getAllReservationByAccommodationId(@PathVariable(name = "accommodationId") Long accommodationId) {
-        return service.getAllReservationByAccommodationId(accommodationId);
-    }
+//    @GetMapping("/{reservationId}")
+//    public void proba(@PathVariable Long reservationId) {
+//        System.out.println("heloooo2");
+//    }
 
-    @GetMapping("/future-reservations/{accommodationId}")
-    public List<Reservation> getFutureReservations(@PathVariable(name = "accommodationId") Long accommodationId) {
-        return service.getAllFutureReservationByAccommodationId(accommodationId);
-    }
 
+//    @GetMapping("/by-accommodation/{accommodationId}")
+//    public List<Reservation> getAllReservationByAccommodationId(@PathVariable(name = "accommodationId") Long accommodationId) {
+//        return service.getAllReservationByAccommodationId(accommodationId);
+//    }
+
+//    @GetMapping("/future-reservations/{accommodationId}")
+//    public List<Reservation> getFutureReservations(@PathVariable(name = "accommodationId") Long accommodationId) {
+//        return service.getAllFutureReservationByAccommodationId(accommodationId);
+//    }
+//
     @GetMapping("/by-guest/{guestId}")
     public List<Reservation> getAllReservationByGuestId(@PathVariable(name = "guestId") Long guestId) {
         return service.getAllReservationByGuestId(guestId);
     }
+//
+//    @PostMapping("/")
+//    public void saveNewReservation(@RequestBody ReservationDTO reservationDTO) {
+//        service.saveNewReservation(reservationDTO);
+//    }
+//
 
-    @PostMapping("/")
-    public void saveNewReservation(@RequestBody ReservationDTO reservationDTO) {
-        service.saveNewReservation(reservationDTO);
-    }
+//
+//    @DeleteMapping(value = "/delete/{reservationId}")
+//    public void deleteReservation(@PathVariable Long reservationId) {
+//        System.out.println("helloo delete");
+//        service.deleteReservation(reservationId);
+//    }
 
-    @DeleteMapping("/{reservationId}")
-    public void deleteReservation(@PathVariable(name = "reservationId") Long reservationId) {
-        service.deleteReservation(reservationId);
-    }
+//    @RequestMapping(value = "/{reservationId}", method = {RequestMethod.DELETE, RequestMethod.GET})
+//    public void deleteReservation(@PathVariable Long reservationId) {
+//        System.out.println("helloo delete");
+//        service.deleteReservation(reservationId);
+//    }
 
-    @PutMapping("/{reservationId}")
-    public void updateDate(@PathVariable(name = "reservationId") Long reservationId, @RequestBody DateIntervalDTO newDate) {
+//
+    @RequestMapping(value = "/{reservationId}", method = RequestMethod.DELETE)
+    public ResponseEntity updateDate(@PathVariable("reservationId") Long reservationId, @RequestBody DateIntervalDTO newDate) {
         service.updateDate(reservationId, newDate);
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
 }
