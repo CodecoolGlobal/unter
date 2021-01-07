01a39d8df58e100a23bceb1bb2a8c305bc800ab0
dr. Simon Bálint <balladin0@gmail.com>
2021-01-07 14:04:03 +0100

    Solution tryouts for Zuul gateway problems

--- gateway/src/main/java/com/codecool/earthbnb/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/codecool/earthbnb/gateway/GatewayApplication.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +29,7 @@ import java.util.Collections;
 @RequiredArgsConstructor
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableDiscoveryClient
 @EnableSwagger2
 public class GatewayApplication{
 
