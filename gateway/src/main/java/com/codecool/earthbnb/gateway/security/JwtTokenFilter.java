1e0e17189a3ba5a7cd6cdfcb24991b9ad850d9b8
magfabian <mag.fabian87@gmail.com>
2021-01-11 12:14:18 +0100

    Fix save accommodation

--- gateway/src/main/java/com/codecool/earthbnb/gateway/security/JwtTokenFilter.java
+++ gateway/src/main/java/com/codecool/earthbnb/gateway/security/JwtTokenFilter.java
@@ -1,5 +1,6 @@
 package com.codecool.earthbnb.gateway.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -9,49 +10,58 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class JwtTokenFilter extends GenericFilterBean {
 
-    private JwtTokenServices jwtTokenServices;
+    private final JwtTokenServices jwtTokenServices;
 
-    JwtTokenFilter(JwtTokenServices jwtTokenServices) {
-        this.jwtTokenServices = jwtTokenServices;
-    }
+//    JwtTokenFilter(JwtTokenServices jwtTokenServices) {
+//        this.jwtTokenServices = jwtTokenServices;
+//    }
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("cicaaaaa");
         String token = jwtTokenServices.getTokenFromRequest((HttpServletRequest) req);
+        System.out.println(token);
 
         if (token != null && jwtTokenServices.validateToken(token)) {
             Authentication auth = jwtTokenServices.parseUserFromTokenInfo(token);
             SecurityContextHolder.getContext().setAuthentication(auth);
         }
 
-//        HttpServletRequest request = (HttpServletRequest) req;
-//        HttpServletResponse response = (HttpServletResponse) res;
-
-//
-//        String origin = ((HttpServletRequest) req).getHeader("origin");
-//
-//
-//        ((HttpServletResponse) res).setHeader("Access-Control-Allow-Origin", origin);
-//        ((HttpServletResponse) res).setHeader("Access-Control-Allow-Credentials", "true");
-//        ((HttpServletResponse) res).setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-//        ((HttpServletResponse) res).setHeader("Access-Control-Max-Age", "86400");
-//
-//
-//        response.addHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
-//        if ("OPTIONS".equals(request.getMethod())) {
-//            response.setStatus(HttpServletResponse.SC_OK);
-//            filterChain.doFilter(req, res);
-//        } else {
-//            filterChain.doFilter(req, res);
-//        }
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
 
 
+        String origin = ((HttpServletRequest) req).getHeader("origin");
 
-       filterChain.doFilter(req, res);
+
+        ((HttpServletResponse) res).setHeader("Access-Control-Allow-Origin", origin);
+        ((HttpServletResponse) res).setHeader("Access-Control-Allow-Credentials", "true");
+        ((HttpServletResponse) res).setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        ((HttpServletResponse) res).setHeader("Access-Control-Max-Age", "86400");
+
+        System.out.println("All names:");
+        System.out.println(((HttpServletResponse) res).getHeaderNames());
+
+        System.out.println("origin");
+        System.out.println(((HttpServletResponse) res).getHeaders("Access-Control-Allow-Origin"));
+
+        response.addHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        if ("OPTIONS".equals(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+            filterChain.doFilter(req, res);
+        } else {
+            filterChain.doFilter(req, res);
+        }
+
+
+
+//       filterChain.doFilter(req, res);
 
     }
 
