b8f31e103ce702b8df5a8f280ea5511c59d5883e
magfabian <mag.fabian87@gmail.com>
2021-01-12 16:46:46 +0100

    Implements methods to find reviews

--- review/src/main/java/com/codecool/review/service/DAO/ReviewDB.java
+++ review/src/main/java/com/codecool/review/service/DAO/ReviewDB.java
@@ -1,4 +1,27 @@
 package com.codecool.review.service.DAO;
 
+import com.codecool.review.model.entity.Review;
+import com.codecool.review.repository.ReviewRepository;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Data
+@RequiredArgsConstructor
 public class ReviewDB implements ReviewDAO {
+
+    private final ReviewRepository repository;
+
+    @Override
+    public List<Review> findAllReviews() {
+        return repository.findAll();
+    }
+
+    @Override
+    public List<Review> findAllReviewsByAccommodationId(Long accommodationId) {
+        return repository.findAllByAccommodationId(accommodationId);
+    }
 }
