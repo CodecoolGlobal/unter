5e192fd7bfea078441039d99ef3027983d1946c3
magfabian <mag.fabian87@gmail.com>
2021-01-12 16:46:34 +0100

    Define methods to find reviews

--- review/src/main/java/com/codecool/review/service/DAO/ReviewDAO.java
+++ review/src/main/java/com/codecool/review/service/DAO/ReviewDAO.java
@@ -1,4 +1,11 @@
 package com.codecool.review.service.DAO;
 
+import com.codecool.review.model.entity.Review;
+
+import java.util.List;
+
 public interface ReviewDAO {
+
+    List<Review> findAllReviews();
+    List<Review> findAllReviewsByAccommodationId(Long accommodationId);
 }
