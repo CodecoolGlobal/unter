fb772ec5d56416b0bad8686785b340ed137f4053
magfabian <mag.fabian87@gmail.com>
2021-01-12 16:48:34 +0100

    Start implementing ReviewService

--- review/src/main/java/com/codecool/review/service/ReviewService.java
+++ review/src/main/java/com/codecool/review/service/ReviewService.java
@@ -1,4 +1,27 @@
 package com.codecool.review.service;
 
+import com.codecool.review.exception.NoDataFoundException;
+import com.codecool.review.model.entity.Review;
+import com.codecool.review.service.DAO.ReviewDAO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewDAO reviewDAO;
+
+    public List<Review> findAllReviews() {
+        return reviewDAO.findAllReviews();
+    }
+
+    public List<Review> findAllReviewsByAccommodationId(Long accommodationId) {
+        if (reviewDAO.findAllReviewsByAccommodationId(accommodationId) == null) {
+            throw new NoDataFoundException();
+        }
+        return reviewDAO.findAllReviewsByAccommodationId(accommodationId);
+    }
 }
