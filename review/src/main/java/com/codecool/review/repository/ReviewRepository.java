d3270cd1e8540db2894af575cee1d396bb09f6df
magfabian <mag.fabian87@gmail.com>
2021-01-12 16:47:47 +0100

    Define method to find all reviews by accommodation id

--- review/src/main/java/com/codecool/review/repository/ReviewRepository.java
+++ review/src/main/java/com/codecool/review/repository/ReviewRepository.java
@@ -1,4 +1,13 @@
 package com.codecool.review.repository;
 
-public interface ReviewRepository {
+import com.codecool.review.model.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findAllByAccommodationId(Long id);
 }
