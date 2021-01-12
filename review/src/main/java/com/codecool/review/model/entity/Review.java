6ead2af050b20603d8b05ca1fafd640f548f1e32
magfabian <mag.fabian87@gmail.com>
2021-01-12 16:45:09 +0100

    Implement Review entity

--- review/src/main/java/com/codecool/review/model/entity/Review.java
+++ review/src/main/java/com/codecool/review/model/entity/Review.java
@@ -1,4 +1,33 @@
 package com.codecool.review.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long accommodationId;
+
+    @Column(nullable = false)
+    private Long guestId;
+
+    @Column(nullable = false)
+    private Double rating;
+
+    @Column(nullable = false)
+    private String message;
+
 }
