f6c4b0c3ef983d989abe0c0b9cc94c8351a82056
magfabian <mag.fabian87@gmail.com>
2021-01-12 16:46:04 +0100

    Add endpoints to ReviewController to find Reviews

--- review/src/main/java/com/codecool/review/controller/ReviewController.java
+++ review/src/main/java/com/codecool/review/controller/ReviewController.java
@@ -1,4 +1,27 @@
 package com.codecool.review.controller;
 
+import com.codecool.review.model.entity.Review;
+import com.codecool.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService service;
+
+    @GetMapping
+    public List<Review> getAll() {
+        return service.findAllReviews();
+    }
+
+    @GetMapping("/accommodation-id/{accommodationId}")
+    public List<Review> getAllReviewByAccommodationId(@PathVariable(name = "accommodationId") Long accommodationId) {
+        return service.findAllReviewsByAccommodationId(accommodationId);
+    }
 }
