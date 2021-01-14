e754d4b238dcc7f24bbe477da4ead9dadf8eabec
magfabian <mag.fabian87@gmail.com>
2021-01-14 15:53:29 +0100

    Define methods to menage reviews

--- review/src/main/java/com/codecool/review/service/DAO/ReviewDAO.java
+++ review/src/main/java/com/codecool/review/service/DAO/ReviewDAO.java
@@ -1,11 +1,22 @@
 package com.codecool.review.service.DAO;
 
-import com.codecool.review.model.entity.Review;
-
+import com.codecool.review.model.DTO.ReviewRequestDTO;
+import com.codecool.review.model.DTO.ReviewResponseDTO;
+import com.codecool.review.model.DTO.ReviewUpdateDTO;
 import java.util.List;
 
 public interface ReviewDAO {
 
-    List<Review> findAllReviews();
-    List<Review> findAllReviewsByAccommodationId(Long accommodationId);
+    List<ReviewResponseDTO> findAllReviews();
+    List<ReviewResponseDTO> findAllReviewsByAccommodationId(Long accommodationId);
+    void saveNewReview(ReviewRequestDTO reviewRequestDTO);
+    ReviewResponseDTO findReviewById(Long reviewId);
+    List<ReviewResponseDTO> findAllReviewsByGuestId(Long guestId);
+    void updateReview(Long reviewId, ReviewUpdateDTO reviewUpdateDTO);
+    boolean reviewExistsById(Long reviewId);
+    void deleteReviewById(Long reviewId);
+    void deleteAllReviewsByAccommodationId(Long accommodationId);
+    void deleteAllReviewsByGuestId(Long guestId);
+    boolean existsByAccommodationId(Long accommodationId);
+    boolean existsByGuestId(Long guestId);
 }
