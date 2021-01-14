928d6c9f13e58983a049fb603cc4bc8d34fc9867
magfabian <mag.fabian87@gmail.com>
2021-01-14 15:51:06 +0100

    Remove unnecessary import

--- review/src/main/java/com/codecool/review/ReviewApplication.java
+++ review/src/main/java/com/codecool/review/ReviewApplication.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
@@ -16,5 +15,4 @@ public class ReviewApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReviewApplication.class, args);
     }
-
 }
