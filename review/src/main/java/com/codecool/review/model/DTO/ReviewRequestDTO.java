290e3d7f5254bfdc7dfbe9a549859b8585f196ea
magfabian <mag.fabian87@gmail.com>
2021-01-14 15:55:24 +0100

    Add ReviewRequestDTO fields

--- review/src/main/java/com/codecool/review/model/DTO/ReviewRequestDTO.java
+++ review/src/main/java/com/codecool/review/model/DTO/ReviewRequestDTO.java
@@ -1,4 +1,24 @@
 package com.codecool.review.model.DTO;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ReviewRequestDTO {
+
+    private Long accommodationId;
+    private Long guestId;
+    private Double rating;
+    private String message;
+
+    public ReviewRequestDTO() {
+    }
+
+    public ReviewRequestDTO(Long accommodationId, Long guestId, Double rating, String message) {
+        this.accommodationId = accommodationId;
+        this.guestId = guestId;
+        this.rating = rating;
+        this.message = message;
+    }
 }
