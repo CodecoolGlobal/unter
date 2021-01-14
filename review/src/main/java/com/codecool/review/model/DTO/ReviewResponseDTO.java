7de910b1b404607191271929692030ed33780fd9
magfabian <mag.fabian87@gmail.com>
2021-01-14 15:55:41 +0100

    Add ReviewResponseDTO fields

--- review/src/main/java/com/codecool/review/model/DTO/ReviewResponseDTO.java
+++ review/src/main/java/com/codecool/review/model/DTO/ReviewResponseDTO.java
@@ -1,4 +1,23 @@
 package com.codecool.review.model.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReviewResponseDTO {
+
+    private Long id;
+    private Long accommodationId;
+    private Long guestId;
+    private Double rating;
+    private String message;
+    private LocalDate date;
+
 }
