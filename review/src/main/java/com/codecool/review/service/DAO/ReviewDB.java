b35dc0dd00119889be3c6048a12dd9ea326daf1e
magfabian <mag.fabian87@gmail.com>
2021-01-14 15:53:50 +0100

    Add field

--- review/src/main/java/com/codecool/review/service/DAO/ReviewDB.java
+++ review/src/main/java/com/codecool/review/service/DAO/ReviewDB.java
@@ -1,12 +1,20 @@
 package com.codecool.review.service.DAO;
 
+import com.codecool.review.exception.ReviewNotFoundException;
+import com.codecool.review.model.DTO.ReviewRequestDTO;
+import com.codecool.review.model.DTO.ReviewResponseDTO;
+import com.codecool.review.model.DTO.ReviewUpdateDTO;
 import com.codecool.review.model.entity.Review;
 import com.codecool.review.repository.ReviewRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 @Data
@@ -14,6 +22,8 @@ import java.util.List;
 public class ReviewDB implements ReviewDAO {
 
     private final ReviewRepository repository;
+
+    private List<ReviewResponseDTO> reviewResponseDTOList;
 
     @Override
     public List<Review> findAllReviews() {
