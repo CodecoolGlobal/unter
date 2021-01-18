82136280a17b5ed13e70278b5ce14584e2db50bc
magfabian <mag.fabian87@gmail.com>
2021-01-18 18:22:22 +0100

    Add new test to test validation

--- review/src/test/java/com/codecool/review/controller/ReviewControllerTest.java
+++ review/src/test/java/com/codecool/review/controller/ReviewControllerTest.java
@@ -189,6 +189,126 @@ public class ReviewControllerTest {
             .andDo(print());
     }
 
+    @Test
+    public void test_reviewMessageFieldContainsSpecialCharacters_statusIs400() throws Exception {
+        ReviewRequestDTO testReviewRequestDTO2 = ReviewRequestDTO.builder()
+            .accommodationId(2L)
+            .guestId(2L)
+            .rating(1.00)
+            .message("<>;")
+            .build();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                .put("/")
+                .characterEncoding("utf-8")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(testReviewRequestDTO2)))
+            .andExpect(status().is4xxClientError())
+            .andDo(print());
+    }
+
+    @Test
+    public void test_reviewMessageFieldTooLong_statusIs400() throws Exception {
+        ReviewRequestDTO testReviewRequestDTO2 = ReviewRequestDTO.builder()
+            .accommodationId(2L)
+            .guestId(2L)
+            .rating(1.00)
+            .message("RegExr was created by gskinner.com, and is proudly hosted by Media Temple. Edit the Expression & Text to see matches. Roll over matches or the expression for details. PCRE & JavaScript flavors of RegEx are supported. Validate your expression with Tests mode. The side bar includes a Cheatsheet, full Reference, and Help. You can also Save & Share with the Community, and view patterns you create or favorite in My Patterns. Explore results with the Tools below. Replace & List output custom results. Details lists capture groups. Explain describes your expression in plain English.")
+            .build();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                .put("/")
+                .characterEncoding("utf-8")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(testReviewRequestDTO2)))
+            .andExpect(status().is4xxClientError())
+            .andDo(print());
+    }
+
+    @Test
+    public void test_reviewAccommodationIdShouldBeNotNull_statusIs400() throws Exception {
+        ReviewRequestDTO testReviewRequestDTO2 = ReviewRequestDTO.builder()
+            // missing accommodation id field
+            .guestId(2L)
+            .rating(1.00)
+            .message("testReviewRequestDTO2")
+            .build();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                .put("/")
+                .characterEncoding("utf-8")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(testReviewRequestDTO2)))
+            .andExpect(status().is4xxClientError())
+            .andDo(print());
+    }
+
+    @Test
+    public void test_reviewGuestIdShouldBeNotNul_statusIs400() throws Exception {
+        ReviewRequestDTO testReviewRequestDTO2 = ReviewRequestDTO.builder()
+            .accommodationId(2L)
+            // guest id field is missing
+            .rating(1.00)
+            .message("test")
+            .build();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                .post("/")
+                .characterEncoding("utf-8")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(testReviewRequestDTO2)))
+            .andExpect(status().is4xxClientError())
+            .andDo(print());
+    }
+
+    @Test
+    public void test_reviewRatingFieldShouldBeNotNull_statusIs400() throws Exception {
+        ReviewRequestDTO testReviewRequestDTO2 = ReviewRequestDTO.builder()
+            .accommodationId(2L)
+            .guestId(2L)
+            // rating field is missing
+            .message("test")
+            .build();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                .post("/")
+                .characterEncoding("utf-8")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(testReviewRequestDTO2)))
+            .andExpect(status().is4xxClientError())
+            .andDo(print());
+    }
+
+    @Test
+    public void test_reviewMessageFieldCanBeNull_statusIsOK() throws Exception {
+        ReviewRequestDTO testReviewRequestDTO2 = ReviewRequestDTO.builder()
+            .accommodationId(2L)
+            .guestId(2L)
+            .rating(1.00)
+            // missing message field
+            .build();
+
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                .post("/")
+                .characterEncoding("utf-8")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(testReviewRequestDTO2)))
+            .andExpect(status().isOk())
+            .andDo(print());
+    }
+
     public static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
