ef3a72491800aca07e82324c6fc380c11585da0a
magfabian <mag.fabian87@gmail.com>
2021-01-18 18:23:51 +0100

    Add annotation ti validate fields of dto

--- review/src/main/java/com/codecool/review/model/DTO/ReviewResponseDTO.java
+++ review/src/main/java/com/codecool/review/model/DTO/ReviewResponseDTO.java
@@ -1,10 +1,14 @@
 package com.codecool.review.model.DTO;
 
+import com.codecool.review.validation.annotation.ValidLong;
+import com.codecool.review.validation.annotation.ValidMessage;
+import com.codecool.review.validation.annotation.ValidRating;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 @Data
@@ -13,11 +17,22 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class ReviewResponseDTO {
 
+    @ValidLong(message = "Review Id cannot be null")
     private Long id;
+
+    @ValidLong(message = "Accommodation Id cannot be null")
     private Long accommodationId;
+
+    @ValidLong(message = "Guest / User Id cannot be null")
     private Long guestId;
+
+    @ValidRating
     private Double rating;
+
+    @ValidMessage
     private String message;
+
+    @NotNull(message = "Date cannot be null")
     private LocalDate date;
 
 }
