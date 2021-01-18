e2e96e7129308aaaf72e30c957590f8a8321d4c0
magfabian <mag.fabian87@gmail.com>
2021-01-18 18:18:31 +0100

    Implement MessageValidator

---
+++ review/src/main/java/com/codecool/review/validation/MessageValidator.java
@@ -0,0 +1,22 @@
+package com.codecool.review.validation;
+
+import com.codecool.review.validation.annotation.ValidMessage;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class MessageValidator implements ConstraintValidator<ValidMessage, String> {
+
+    @Override
+    public boolean isValid(String message, ConstraintValidatorContext context) {
+        return containsValidChars(message);
+    }
+
+    public boolean containsValidChars(String message) {
+        if (message != null) {
+            String regex = "^[a-zA-Z0-9\\s.:,!?()]{1,255}$";
+            return message.matches(regex);
+        }
+        return true;
+    }
+}
